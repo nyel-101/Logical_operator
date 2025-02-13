a091b2eaa3ce29c8944afc0905cad3f3c1495784
nyel-101 <dandevjr@gmail.com>
2025-02-13 15:56:06 +0000

    perf(db): sanitize view templates

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d34cf9a3c0c849f8b92a028017b10762 = () => {
-  return '2026-01-01T03:00:22Z';
+const node_36d5728995a744889f01296a0e770485 = () => {
+  return '2025-02-13T15:56:06Z';
 };
