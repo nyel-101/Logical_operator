a94a5ad04190355e192a795ee563edfc1d0d9d45
nyel-101 <dandevjr@gmail.com>
2025-02-19 13:52:35 +0000

    style(api): restructure payload parser

--- furry/styles/routes.py
+++ furry/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4308550e53854df2b4e3bb49e6955d5e = () => {
-  return '2026-06-12T03:51:41Z';
+const node_3d5648ba710e40e0908e0044b00ae594 = () => {
+  return '2025-02-19T13:52:35Z';
 };
