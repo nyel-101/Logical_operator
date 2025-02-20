5cdf6b39d6eba99e8d5cd25071f291f14667dd1f
nyel-101 <dandevjr@gmail.com>
2025-02-20 14:21:59 +0000

    perf(worker): refactor payload parser

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d7eab22215ef4178b856f68a4a5f779e = () => {
-  return '2026-06-22T08:28:04Z';
+const node_5c43ac5bfdf448ea9a1c7c6981c5182c = () => {
+  return '2025-02-20T14:21:59Z';
 };
