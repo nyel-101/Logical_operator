81bda891373b8e5360f85f9b6dce8dac2e028910
nyel-101 <dandevjr@gmail.com>
2025-03-01 10:39:16 +0000

    feat(core): optimize middleware routes

--- fury/src/utils/routes.py
+++ fury/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3bdc256e82994ab3afd3347c4699f5e6 = () => {
-  return '2026-01-07T01:46:54Z';
+const node_42ab5c00ae5c4239ba771911f86804a7 = () => {
+  return '2025-03-01T10:39:16Z';
 };
