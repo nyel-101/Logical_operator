088da2db1a94765110c725999aae04c14ffc4d9b
nyel-101 <dandevjr@gmail.com>
2025-03-11 23:50:21 +0000

    perf(config): restructure session store

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3b3f831b4c54a05b397814c59124085 = () => {
-  return '2025-02-18T05:48:49Z';
+const node_963fb817fb2b49ecb7334e4b4500869f = () => {
+  return '2025-03-11T23:50:21Z';
 };
