e48deba101fce58895c626e2b398427b858447a4
nyel-101 <dandevjr@gmail.com>
2025-03-14 12:52:03 +0000

    chore(auth): enhance database index

--- furry/tests/schema.py
+++ furry/tests/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_45deb14e3bff4479804568ea5c84758d = () => {
-  return '2026-05-13T15:14:08Z';
+const node_18d8df2f3eb24470ace04d8f1c31926d = () => {
+  return '2025-03-14T12:52:03Z';
 };
