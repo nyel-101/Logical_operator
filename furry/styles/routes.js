c5c0e480020c68ae8025fbfbf619901f1372312c
nyel-101 <dandevjr@gmail.com>
2025-03-19 01:49:26 +0000

    fix(auth): refactor view templates

--- furry/styles/routes.js
+++ furry/styles/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_680b882d5b704e2cb308a6265d2122f2 = () => {
-  return '2026-02-16T06:46:09Z';
+const node_fda8859d5da9460abc4d37bcb910b310 = () => {
+  return '2025-03-19T01:49:26Z';
 };
