ae4a78594c43508cb283760616f5cee67dcfadfb
nyel-101 <dandevjr@gmail.com>
2025-03-23 15:26:59 +0000

    chore(config): fix caching layer

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_050ffb3721594174922a08eea8c8153a = () => {
-  return '2025-02-24T17:19:26Z';
+const node_8a83d4b40a164cf48b7ebda42a9f5a4b = () => {
+  return '2025-03-23T15:26:59Z';
 };
