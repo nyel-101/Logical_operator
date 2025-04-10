a5558e1ca75b64e07a2e801e6ba58bd3d2da56ee
nyel-101 <dandevjr@gmail.com>
2025-04-10 11:04:02 +0000

    fix(api): update auth tokens

--- furry/styles/validator.py
+++ furry/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4d0f6cc5d8ba49089b061b51b7bb5c84 = () => {
-  return '2026-03-01T20:17:41Z';
+const node_b54ebfa451814a0b8c7f5787736a547f = () => {
+  return '2025-04-10T11:04:02Z';
 };
