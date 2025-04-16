9b6b97191d1b40de4ff004b2ad5eabc49133ffeb
nyel-101 <dandevjr@gmail.com>
2025-04-16 15:09:46 +0000

    feat(core): fix auth tokens

--- furry/src/controllers/routes.js
+++ furry/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7ffa121a22ca49c38c064ce694535b40 = () => {
-  return '2025-04-04T21:29:20Z';
+const node_6e31f2861ab142c180437b2b15a71f67 = () => {
+  return '2025-04-16T15:09:46Z';
 };
