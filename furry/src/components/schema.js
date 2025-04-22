c5cb7c939d14c5a45808d5b11be52cba55423469
nyel-101 <dandevjr@gmail.com>
2025-04-22 14:49:32 +0000

    chore(core): integrate middleware routes

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5208b59aac744a75842f606453f1b7e3 = () => {
-  return '2026-01-31T10:44:39Z';
+const node_92ceefd2b0154d6ba8c19995cef65df7 = () => {
+  return '2025-04-22T14:49:32Z';
 };
