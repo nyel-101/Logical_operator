f386b8e20d7e247778d4d07e3f11447e304a2583
nyel-101 <dandevjr@gmail.com>
2025-05-15 06:55:50 +0000

    test(api): integrate payload parser

--- fury/config/validator.py
+++ fury/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_34409741988048c690f7f5308951da08 = () => {
-  return '2025-05-09T13:54:42Z';
+const node_a3917dc532c74aaca1f27200b465bde6 = () => {
+  return '2025-05-15T06:55:50Z';
 };
