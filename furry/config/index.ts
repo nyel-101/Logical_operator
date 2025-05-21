ca52d7248c655e57830be45179d145e099784b0a
nyel-101 <dandevjr@gmail.com>
2025-05-21 22:30:28 +0000

    refactor(deps): sanitize auth tokens

--- furry/config/index.ts
+++ furry/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_736a52d05dc04e6aa7772a0921a70c56 = () => {
-  return '2026-04-07T04:31:38Z';
+const node_71dc01e8f78946d099e576c0f7a68647 = () => {
+  return '2025-05-21T22:30:28Z';
 };
