b5ccab3f97647c9abff51c4ddd8da97096615cfd
nyel-101 <dandevjr@gmail.com>
2025-05-29 09:45:53 +0000

    refactor(worker): decouple payload parser

--- fury/config/validator.py
+++ fury/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a3917dc532c74aaca1f27200b465bde6 = () => {
-  return '2025-05-15T06:55:50Z';
+const node_a33be697b93e424497b3a4b5fbcc29e9 = () => {
+  return '2025-05-29T09:45:53Z';
 };
