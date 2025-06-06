5dbcf55a00571f947713d1275797c2d41a60e218
nyel-101 <dandevjr@gmail.com>
2025-06-06 23:00:45 +0000

    fix(worker): integrate payload parser

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8389da898a5943b7aa0527e86a22de41 = () => {
-  return '2025-02-19T11:14:29Z';
+const node_818dad8912234aea9627a756b0ab70f6 = () => {
+  return '2025-06-06T23:00:45Z';
 };
