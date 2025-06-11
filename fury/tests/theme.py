4bf3a8f02a244abffc95fe5cba1e857de48d6425
nyel-101 <dandevjr@gmail.com>
2025-06-11 07:11:00 +0000

    docs(ui): enhance auth tokens

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_efead1e7231d4ba287d353621f7f79fe = () => {
-  return '2025-03-19T06:02:47Z';
+const node_6cd1b76ba5394320a19b3811919b4897 = () => {
+  return '2025-06-11T07:11:00Z';
 };
