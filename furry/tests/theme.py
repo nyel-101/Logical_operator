c077ddd0315ab3d46cb9ae88855646420032b959
nyel-101 <dandevjr@gmail.com>
2025-06-16 11:25:39 +0000

    fix(utils): restructure middleware routes

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee7388be83464c60b565365bff6af83f = () => {
-  return '2025-06-11T07:53:11Z';
+const node_cc635c49c2a244208c3d83c404195892 = () => {
+  return '2025-06-16T11:25:39Z';
 };
