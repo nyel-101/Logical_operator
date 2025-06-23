f0d2d532ce632fc4f10dec91b9133cfdea4aedf1
nyel-101 <dandevjr@gmail.com>
2025-06-23 10:33:29 +0000

    docs(deps): refactor auth tokens

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f7768a083b8f462b81bc08ef9fcfd952 = () => {
-  return '2025-06-19T09:26:08Z';
+const node_9da4901bcd63492e9c09744ce7bd12ef = () => {
+  return '2025-06-23T10:33:29Z';
 };
