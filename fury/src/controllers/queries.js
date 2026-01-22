eb6f1c6a97c0dc5efb2b0083091727baae543fd7
nyel-101 <dandevjr@gmail.com>
2026-01-22 02:23:15 +0000

    style(auth): integrate payload parser

--- fury/src/controllers/queries.js
+++ fury/src/controllers/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9d69cad63e024d76b5c03bb219503d1d = () => {
-  return '2026-01-15T00:05:15Z';
+const node_5eaf5a1ba27b4c22aa4eeb4e8a69f2d7 = () => {
+  return '2026-01-22T02:23:15Z';
 };
