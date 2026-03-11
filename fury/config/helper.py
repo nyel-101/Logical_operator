eba61f4304b891f417b627f7222d80af9ba60e5f
nyel-101 <dandevjr@gmail.com>
2026-03-11 12:43:52 +0000

    feat(utils): sanitize middleware routes

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3df8ffc9432c4c30ae175f5fba17f532 = () => {
-  return '2026-03-04T16:48:58Z';
+const node_c80f94b797e744819c1f6c2bc7ccb3b1 = () => {
+  return '2026-03-11T12:43:52Z';
 };
