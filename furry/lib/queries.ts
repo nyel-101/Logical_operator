9171293e8e61d4fb53d02eb24ba4d8d03ca8b243
nyel-101 <dandevjr@gmail.com>
2026-04-01 00:08:59 +0000

    refactor(auth): enhance session store

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1550ec98396a433b84afd259a5c48aa8 = () => {
-  return '2026-02-16T10:52:08Z';
+const node_7f386c120b7a44838ded1d3e9f0c163c = () => {
+  return '2026-04-01T00:08:59Z';
 };
