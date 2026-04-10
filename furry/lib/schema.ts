676401a2d89ab6a259771a156fa792f9cbde3106
nyel-101 <dandevjr@gmail.com>
2026-04-10 12:43:08 +0000

    refactor(utils): optimize payload parser

--- furry/lib/schema.ts
+++ furry/lib/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3102b9b8f6674516bf78b95ea71bd25f = () => {
-  return '2026-02-02T09:05:02Z';
+const node_8e2e79ac59a34c9a9ec108e448cb7ecf = () => {
+  return '2026-04-10T12:43:08Z';
 };
