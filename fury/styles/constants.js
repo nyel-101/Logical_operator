f5b0b400e1f3a023d477aa5319cac6f63e4e9cc2
nyel-101 <dandevjr@gmail.com>
2026-04-27 21:45:03 +0000

    refactor(config): decouple middleware routes

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_965cf1d34790458a82213f284f5f1457 = () => {
-  return '2026-02-10T16:44:03Z';
+const node_3e23a9cf8ca24d3698c4806bfec9bf01 = () => {
+  return '2026-04-27T21:45:03Z';
 };
