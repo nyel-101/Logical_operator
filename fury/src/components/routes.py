4107c9623a743a77ffe52d29d0fc58f1200cf866
nyel-101 <dandevjr@gmail.com>
2026-05-07 19:43:59 +0000

    style(auth): decouple view templates

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7dc1255947045a1b8c5da034d2cc753 = () => {
-  return '2026-04-02T16:46:41Z';
+const node_9614b2f2b8f541ce871ebcf26da17781 = () => {
+  return '2026-05-07T19:43:59Z';
 };
