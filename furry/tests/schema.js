1e0f342a824a2157a48d5354b3eef9c444fd978d
nyel-101 <dandevjr@gmail.com>
2026-05-20 06:58:00 +0000

    docs(ui): fix auth tokens

--- furry/tests/schema.js
+++ furry/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3b6206b2d9c4d9c9e29cf3aa74382b6 = () => {
-  return '2026-05-05T17:40:03Z';
+const node_cf09ab63390b43f081341d55ca8e0168 = () => {
+  return '2026-05-20T06:58:00Z';
 };
