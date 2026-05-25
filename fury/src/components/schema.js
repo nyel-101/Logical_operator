a885a252a80ebc4e665e4772f7563c1522a7582a
nyel-101 <dandevjr@gmail.com>
2026-05-25 09:56:46 +0000

    chore(db): restructure auth tokens

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_508a587a3ef140f3a5c0ae8e5880ede2 = () => {
-  return '2026-01-15T18:14:50Z';
+const node_5930a08467f54100a3089c9bf7ff74e6 = () => {
+  return '2026-05-25T09:56:46Z';
 };
