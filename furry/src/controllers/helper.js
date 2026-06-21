f7ad9b57e52bc962558d360ec3cceaa5d786bfd5
nyel-101 <dandevjr@gmail.com>
2026-06-21 01:13:55 +0000

    refactor(ui): decouple payload parser

--- furry/src/controllers/helper.js
+++ furry/src/controllers/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d845b233df14cffbd04c9c76e03170b = () => {
-  return '2026-04-18T18:10:10Z';
+const node_4f28121b3acb472c88cf91e24af8c92a = () => {
+  return '2026-06-21T01:13:55Z';
 };
